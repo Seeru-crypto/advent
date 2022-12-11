b84a7a7046d5452237064f52a78f8fd7b9d26f4b
Fred-Andree Oja <fred-andree.oja@netgroup.com>
2022-12-11 14:55:02 +0200

    Add Day 4 solution

--- 2022/4/b.ts
+++ 2022/4/b.ts
@@ -1,6 +1,6 @@
 const dataAsString = await Deno.readTextFile("./data.txt")
-// With test data 2 matches
-// With actual data 462 matches
+// With test data 4 matches
+// With actual data 835 matches
 
 // Formatting input data into more usable format
 const dataAsStrArray = dataAsString.split("\n").map(str => str.split(","));
@@ -10,18 +10,26 @@ let counter = 0;
 // Task logic
 console.log(dataAsNumberArray)
 dataAsNumberArray.forEach(grp => {
+    // grp comes in format [ [1, 2], [4, 5] ]
+    const sectionStartA = grp[0][0];
+    const sectionEndA = grp[0][1];
+    const sectionStartB = grp[1][0];
+    const sectionEndB = grp[1][1];
+
     // oneway check
-    if (grp[0][0] <= grp[1][0] && grp[0][1] >= grp[1][1]){
-        console.log("first if: ", grp)
+    if (isGivenNrInNumberArea(sectionStartA, sectionStartB, sectionEndB )){
         counter += 1
         return;
     }
     // second way check
-    else if (grp[0][0] >= grp[1][0] && grp[0][1] <= grp[1][1]){
-        console.log("second if: ", grp)
+    else if (isGivenNrInNumberArea(sectionStartB, sectionStartA, sectionEndA )){
         counter += 1
         return;
     }
 })
+
+function isGivenNrInNumberArea(num : number, areaStart : number, areaEnd : number): boolean{
+    return areaStart <= num && areaEnd >= num;
+}
 
 console.log({counter})
